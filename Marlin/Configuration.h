55abc753122735fbac9a60ecf7646e3e1184d490
Christoph Müller Philipps Sohn <dev@mps.wtf>
2022-08-31 12:30:57 +0200

    Bed Leveling Corner with Probe

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1828,12 +1828,12 @@
  */
 //#define AUTO_BED_LEVELING_3POINT
 //#define AUTO_BED_LEVELING_LINEAR
-#if WITHOUT_ABL
-  #define MESH_BED_LEVELING
-#else
-  #define AUTO_BED_LEVELING_BILINEAR
-#endif
-//#define AUTO_BED_LEVELING_UBL
+//#if WITHOUT_ABL
+ // #define MESH_BED_LEVELING
+//#else
+//  #define AUTO_BED_LEVELING_BILINEAR
+//#endif
+#define AUTO_BED_LEVELING_UBL
 //#define MESH_BED_LEVELING
 
 /**
@@ -1976,15 +1976,15 @@
 #define LEVEL_BED_CORNERS
 
 #if ENABLED(LEVEL_BED_CORNERS)
-  #define LEVEL_CORNERS_INSET_LFRB { 30, 30, 30, 30 } // (mm) Left, Front, Right, Back insets
+  #define LEVEL_CORNERS_INSET_LFRB { 50, 50, 50, 50 } // (mm) Left, Front, Right, Back insets
   #define LEVEL_CORNERS_HEIGHT      0.0   // (mm) Z height of nozzle at leveling points
   #define LEVEL_CORNERS_Z_HOP       4.0   // (mm) Z height of nozzle between leveling points
   //#define LEVEL_CENTER_TOO              // Move to the center after the last corner
-  //#define LEVEL_CORNERS_USE_PROBE
+  #define LEVEL_CORNERS_USE_PROBE
   #if ENABLED(LEVEL_CORNERS_USE_PROBE)
     #define LEVEL_CORNERS_PROBE_TOLERANCE 0.1
     #define LEVEL_CORNERS_VERIFY_RAISED   // After adjustment triggers the probe, re-probe to verify
-    //#define LEVEL_CORNERS_AUDIO_FEEDBACK
+    #define LEVEL_CORNERS_AUDIO_FEEDBACK
   #endif
 
   /**
